7d13a572cb72fa6d6668164df45be7546f977165
Razadan1 <danmoleomomayowa@gmail.com>
2024-08-14 17:42:49 +0100

    Advanced Task 100 using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# set up your client SSH configuration file so that you can connect to a server without typing a password.
+
+file{ 'etc/ssh/ssh_config':
+        ensure => present,
+content =>"
+
+        #SSH client configuration
+        host*
+        IdentityFile ~/.ssh/school
+        PasswordAunthentication no
+}
