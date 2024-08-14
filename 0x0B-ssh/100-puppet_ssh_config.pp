95edb50acb09c71ddee60366067dcaeba2823056
Razadan1 <danmoleomomayowa@gmail.com>
2024-08-14 17:56:38 +0100

    Task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # set up your client SSH configuration file so that you can connect to a server without typing a password.
 
-file { 'etc/ssh/ssh_config':
+file { 'etc/ssh/ssh_cofig':
         ensure => present,
 content =>"
 
