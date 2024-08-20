f76162c032f98fb0b95348bd3f4fa7d3f6806b77
Razadan1 <danmoleomomayowa@gmail.com>
2024-08-20 15:36:12 +0100

    Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,9 +4,11 @@ Function that queries the Reddit API and prints
 the top ten hot posts of a subreddit
 """
 import requests
+import sys
+
 
 def top_ten(subreddit):
-    """ Queries the Reddit API and prints the top ten hot posts of a subreddit """
+    """ Queries to Reddit API """
     u_agent = 'Mozilla/5.0'
 
     headers = {
@@ -18,61 +20,17 @@ def top_ten(subreddit):
     }
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    res = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
     if res.status_code != 200:
         print(None)
         return
-    
-    try:
-        dic = res.json()
-    except ValueError:
-        # This will catch JSON decoding errors
-        print(None)
-        return
-    
-    hot_posts = dic.get('data', {}).get('children', [])
-    
-    if not hot_posts:  # Simplified the check for an empty list
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    if len(hot_posts) == 0:
         print(None)
     else:
         for post in hot_posts:
             print(post['data']['title'])
-
-
-# #!/usr/bin/python3
-# """
-# Function that queries the Reddit API and prints
-# the top ten hot posts of a subreddit
-# """
-# import requests
-# import sys
-
-
-# def top_ten(subreddit):
-#     """ Queries to Reddit API """
-#     u_agent = 'Mozilla/5.0'
-
-#     headers = {
-#         'User-Agent': u_agent
-#     }
-
-#     params = {
-#         'limit': 10
-#     }
-
-#     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-#     res = requests.get(url,
-#                        headers=headers,
-#                        params=params,
-#                        allow_redirects=False)
-#     if res.status_code != 200:
-#         print(None)
-#         return
-#     dic = res.json()
-#     hot_posts = dic['data']['children']
-#     if len(hot_posts) == 0:
-#         print(None)
-#     else:
-#         for post in hot_posts:
-#             print(post['data']['title'])
