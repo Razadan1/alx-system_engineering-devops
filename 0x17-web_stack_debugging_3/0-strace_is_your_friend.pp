eabde0cb087091b589d1776bc7fdcd297e744d54
Razadan1 <danmoleomomayowa@gmail.com>
2024-08-27 18:04:22 +0100

    Task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
-# A puppet code that fixes 
+# A puppet code that fixes a word press server
 
 exec {
     'fix-wordpress-server-issue':
     command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
-    path    => 'usr/bin/sed',
+    path    => ['usr/bin/sed'],
 }
