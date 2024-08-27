4020ce10a04a760ce58739be185ced48b944e095
Razadan1 <danmoleomomayowa@gmail.com>
2024-08-27 18:13:24 +0100

    Task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # A puppet code that fixes a word press server
 
-exec {
-    'fix-wordpress-server-issue':
-    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
-    path    => ['/usr/bin/sed'],
+exec { 'fix-wordpress':
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+/var/www/html/wp-settings.php; service apache2 restart"',
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
