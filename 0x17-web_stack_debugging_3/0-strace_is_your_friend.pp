268e5323ed7f6e38e400e96ec13407c36ebdd18f
Razadan1 <danmoleomomayowa@gmail.com>
2024-08-27 17:34:40 +0100

    Task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,6 @@
 
 exec {
     'fix-wordpress-server-issue':
-    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path => 'usr/bin/sed'
 }
