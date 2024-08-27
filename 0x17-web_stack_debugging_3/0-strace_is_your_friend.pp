2bdd63cfdc01dc28b79841da21a1c553e0e3a7ec
Razadan1 <danmoleomomayowa@gmail.com>
2024-08-27 17:16:01 +0100

    Task 0 done

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# A puppet code that fixes 
+
+exec {
+    'fix-wordpress-server-issue':
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
+    path    => '/usr/bin/sed',
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,2 @@
+GitHub repository: alx-system_engineering-devops
+Directory: 0x17-web_stack_debugging_3
